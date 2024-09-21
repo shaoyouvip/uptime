99bf2280a3b97ff0bb56283995ee7f59b9f0636a
周润发 <shaoyouvip@gmail.com>
2024-09-21 14:46:01 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,31 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '周润发的在线状态',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur2671488-fb491a80091166a48bc66b17',
+    //'m797638073-924f5efed86446cce1c95ae1', // 周润发的博客 API key
+    //'m797713213-1c83620d56db599960ad0244', // 周润发的公益订阅 API key
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'GitHub',
+      url: 'https://github.com/shaoyouvip'
+    },
+    {
+      text: 'Blog',
+      url: 'https://blog.001315.xyz/'
+    },
+  ],
+};
