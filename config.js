5c2c76e31d58ad800a033ab9558b3dd4d76d7386
gpter <admin@i.pkuschool.edu.cn>
2024-10-11 20:45:20 +0800

    Update config.js

--- config.js
+++ config.js
@@ -6,10 +6,7 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'ur2671488-fb491a80091166a48bc66b17',
-    //'m797638073-924f5efed86446cce1c95ae1', // 周润发的博客 API key
-    //'m797713213-1c83620d56db599960ad0244', // 周润发的公益订阅 API key
-    //'m797713331-835664f191b589688f122e00', // 周润发的公益订阅器 API key
+    'ur2702718-9d755ba29d35ba4b1e603a70',
   ],
 
   // 日志天数
@@ -17,20 +14,7 @@ window.Config = {
 
   // 是否显示检测站点的链接
   ShowLink: true,
-
-  // 导航栏菜单
-  Navi: [
-    {
-      text: '主页',
-      url: 'https://uptime.001315.xyz/'
-    },    
-    {
-      text: 'GitHub',
-      url: 'https://github.com/shaoyouvip/uptime/'
-    },
-    {
-      text: '博客',
-      url: 'https://blog.aizrf.COM/'
-    },
-  ],
+  
 };
+
+window.Config = {
